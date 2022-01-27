1dbc5dd0d2f8a3329fee5170da21135ebd91ee48
Yago Souza Ramos <yago.ramos@lead.fortaleza>
2022-01-27 16:47:46 -0300

    commit final 27-01

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Observable<Course[]>;
+  // courses: Course[] = [];
+  displayedColumns = ['name','category'];
+
+  // CoursesService: CoursesService;
+
+  constructor(private CoursesService: CoursesService) {
+    // this.courses = [];
+    // this.coursesService = new CoursesService();
+    this.courses = this.CoursesService.list();
+
+    // this.CoursesService.list().subscribe(courses => this.courses = courses);
+  }
 
   ngOnInit(): void {
+
   }
 
 }
